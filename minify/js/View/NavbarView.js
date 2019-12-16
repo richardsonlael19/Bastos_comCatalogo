ad7ba49c310c840230e62caf6d7320b66a40b757
richardsonlael19 <54638966+richardsonlael19@users.noreply.github.com>
2019-12-16 15:14:53 -0300

    navbar atualizado

--- minify/js/View/NavbarView.js
+++ minify/js/View/NavbarView.js
@@ -1,2 +1,72 @@
-class NavbarView extends View { constructor(l) { super(l), this.logoImage = "img/Logomarca.webp", this.lupaImage = "img/Lupa.webp", this.redes_sociais = new RedesView(document.querySelector("#redes_sociais")) }
-    template() { return `\n                \n                <div class="navbar sticky-top navbar-light bg-light shadow-lg ">\n        \n                <a class="navbar-brand logo-mobile" href="index.html"><img class="logo" src="${this.logoImage}" alt=""></a>\n                <a href="#" data-target="slide-out"  class="sidenav-trigger mr-4 sidebar-button"><i class="fa fa-bars" aria-hidden="true" ></i>  </a>\n                \n                <div class="pesquisa-desktop pesquisa-mobile">\n                <p class="pesquisa" align="center">Encontre nossos produtos pela placa.</p>\n                        <div class="row" align="center">\n        \n                        <form onsubmit="controle.enviaDesktop(event)"  class="form-inline col-sm-12 my-2 my-lg-0 font-dark  center-menu">\n                               <input  style="type="text" class="form-control buscaInput col-md-3 col-4  mr-3 placa bordainput input" id="placa" placeholder="PLACA "> \n                                <input style="type="text" class="form-control buscaInput col-md-3 col-4 mr-3 produto bordainputproduto input" id="produto" placeholder="PRODUTO" />\n                                <button class=" col-2 mr-1 btn btn-light">\n                                Buscar\n                                </button>\n                                <a  href="#catalogo" class="col-2 ml-2 btn btn-light btn-sm pesquisa-avancada">Pesquisa Avançada </a>\n                                <br>\n                    \n                                </form>\n                      \n        \n                      </div>\n                      </div>\n                      <div>\n                      <ul class="nav desktop  justify-content-end">\n                     \n                      <li class="nav-item "> \n                      <a class="nav-link " id="pills-profile-tab"  href="index.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold; position: relative; left:80px;"> INÍCIO </a>\n                      </li> \n                      <li class="nav-item "> \n                      <a class="nav-link " id="pills-profile-tab"  href="http://www.c123.com.br/bastos/" role="tab" aria-controls="pills-profile" aria-selected="false"  style="color:grey; font-weight: bold; position: relative; left:63px;"> CATÁLOGO ONLINE </a>\n                      </li> \n                        <li class="nav-item ">\n                        <a class="nav-link " id="pills-profile-tab" href="sobre.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold; position: relative; left:47px;"> SOBRE NÓS</a>\n                        </li>\n                        <li class="nav-item ">\n                        <a class="nav-link " id="pills-profile-tab" href="download.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold; position: relative; left:30px;"> DOWNLOAD</a>\n                        </li>\n                        <li class="nav-item ">\n                        <a class="nav-link " id="pills-profile-tab" href="contato.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold; position: relative; left:10px;"> CONTATO </a>\n                        </li>  \n                        </ul> \n                      <ul id="slide-out" class="sidenav shadow-lg " style=" list-style-type: none; ">\n                 \n                      <li ><a class="waves-effect" href="index.html">INÍCIO</a></li>\n                      <li ><a class="waves-effect" href="#catalogo">PESQUISA</a></li>\n                      <li ><a class="waves-effect" href="http://www.c123.com.br/bastos/"> CATÁLOGO ONLINE</a></li>\n                      <li ><a class="waves-effect" href="sobre.html">SOBRE NÓS</a></li>\n                      <li ><a class="waves-effect" href="download.html">DOWNLOAD</a></li>\n                      <li ><a class="waves-effect" href="contato.html"> CONTATO</a></li>\n                      <li><div class="divider"></div></li>\n                     ${this.redes_sociais.template()}\n                   \n                    </ul>\n                   \n                    <script>\n                if(window.innerWidth < 1025){\n               $(".form-inline").removeClass("center-menu");\n        }\n            <\/script>\n        \n                    ` } }
+class NavbarView extends View {
+    constructor(elemento) {
+        super(elemento)
+        this.logoImage = "img/Logomarca.webp";
+        this.lupaImage = "img/Lupa.webp";
+        this.redes_sociais = new RedesView(document.querySelector("#redes_sociais"))
+    }
+    template() { return `
+    <div class="navbar sticky-top navbar-light bg-light shadow-lg ">
+    
+    <a class="align-middle navbar-brand mx-auto" href="index.html"><img class="align-middle" src="${this.logoImage}" width="140" alt=""></a>
+    <a href="#" data-target="slide-out"  class="sidenav-trigger mr-4 sidebar-button"><i class="fa fa-bars" aria-hidden="true"></i>  </a>
+        <div class=" cormenuplaca">
+        <br>   
+        <p class="mx-auto text-center align-text-bottom fontplaca"><strong>Encontre nossos produtos pela placa.</strong></p>
+                <div class="row mx-auto" >
+                <form onsubmit="controlle.envia(event)" class="form-inline col-sm-12 my-2 my-lg-0 font-dark ">
+                       <input  type="text" class="form-control buscaInput col-md-4 col-3  ml-3 placa bordainput input mx-auto"" id="placa" placeholder="PLACA "> 
+                        <input type="text" class="form-control buscaInput col-md-4 col-4 ml-3 produto bordainputproduto input mx-auto"" id="produto" placeholder="PRODUTO" />
+                        <button  type="submit" class="button-pesquisa buscaInputMobile  btn btn-light ml-2 col-2">Buscar</button> <br> <br>
+                        </form>
+              </div>
+              </div>
+              <div class="mx-auto">
+              <ul class="nav desktop  justify-content-center mx-auto">
+              <li class="nav-item "> 
+              <a class="nav-link "  href="index.html" role="tab" aria-controls="pills-profile"   aria-selected="false" style="color:grey; font-weight: bold;"> 
+              INÍCIO 
+              </a>
+              </li> 
+              <li class="nav-item "> 
+              <a class="nav-link "  href="http://www.c123.com.br/bastos/" role="tab" aria-controls="pills-profile" aria-selected="false"  style="color:grey; font-weight: bold;">
+              CATÁLOGO ONLINE
+               </a>                  
+               </li> 
+                <li class="nav-item ">
+                <a class="nav-link "  href="sobre.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold;">
+                SOBRE
+                </a>                    
+                </li>
+                <li class="nav-item ">
+                <a class="nav-link " href="download.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold;">
+                DOWNLOAD
+                </a>
+                </li>
+                <li class="nav-item ">
+                <a class="nav-link " href="contato.html" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:grey; font-weight: bold;">
+                CONTATO 
+                </a>
+                </li>  
+                </ul> 
+           
+                  <ul id="slide-out" class="sidenav shadow-lg " style=" list-style-type: none; ">
+             
+                  <li ><a class="waves-effect" href="index.html">INÍCIO</a></li>
+                  <li ><a class="waves-effect" href="http://www.c123.com.br/bastos/"> CATÁLOGO ONLINE</a></li>
+                  <li ><a class="waves-effect" href="sobre.html">SOBRE NÓS</a></li>
+                  <li ><a class="waves-effect" href="download.html">DOWNLOAD</a></li>
+                  <li ><a class="waves-effect" href="contato.html"> CONTATO</a></li>
+                  <li><div class="divider"></div></li>
+                 ${this.redes_sociais.template() /* <---- aqui estou re-executando apenas template */}
+               
+                </ul>
+               
+                <script>
+            if(window.innerWidth < 1025){
+           $(".form-inline").removeClass("center-menu");
+    }
+        </script>
+    
+                ` }
+}
